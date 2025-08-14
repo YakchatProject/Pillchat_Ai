95581dab790870a77d8af98c4a8c14b31f606564
kdy071115 <kdy071115@naver.com>
2025-08-14 15:46:01 +0900

    spring 서버 연결 및 토큰

--- services/visualize.py
+++ services/visualize.py
@@ -3,17 +3,11 @@ from PIL import Image
 
 def visualize_ocr_result(image_path: str, ocr_result, save_path: str = "ocr_result.jpg"):
     image = Image.open(image_path).convert("RGB")
-
     boxes = [line[0] for line in ocr_result[0]]
     txts = [line[1][0] for line in ocr_result[0]]
     scores = [line[1][1] for line in ocr_result[0]]
-
-    # 한글 폰트 경로를 지정해야 텍스트가 깨지지 않음
-    annotated = draw_ocr(
-        image, boxes, txts, scores,
-        font_path="fonts/NanumGothic.ttf"  # ← 폰트 파일 경로 수정 가능
-    )
-
+    annotated = draw_ocr(image, boxes, txts, scores, font_path="fonts/NanumGothic.ttf")
+    # Image.fromarray(annotated).save(save_path)
     result_image = Image.fromarray(annotated)
     result_image.save(save_path)
     print(f"[🖼️ OCR 시각화 저장 완료] {save_path}")
